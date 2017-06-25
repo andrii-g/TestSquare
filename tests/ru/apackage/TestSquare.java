368aa0d0666ebee6e5927592f0e926c36635d108
andrii <andrii.grygoruk@gmail.com>
2017-06-26 00:10:48 +0300

    TestSquare

--- tests/ru/apackage/TestSquare.java
+++ tests/ru/apackage/TestSquare.java
@@ -13,28 +13,13 @@ public class TestSquare {
     final double DELTA = 1e-5;
 
     @Test
-    public void checkSideLengthPositive(){
+    public void verifySideLength(){
         Square square = SquareFactory.newSquare(5.11);
         assertEquals("Expected value is not equal to actual", 5.11, square.sideLength(), DELTA);
     }
 
     @Test
-    public void checkSideLengthZero(){
-        Square square = SquareFactory.newSquare(0);
-        assertEquals("Expected value is not equal to actual", 0, square.sideLength(), DELTA);
-    }
-
-    /*
-     In my opinion, test expected result should be some Exception, if provided sideLength is negative. Nevertheless, it should be specified in specification
-     */
-    @Test(expected = Exception.class)
-    public void checkSideLengthNegative(){
-        Square square = SquareFactory.newSquare(-15.99);
-        square.sideLength();
-    }
-
-    @Test
-    public void checkSquareOfSquarePositive(){
+    public void verifySquareOfSquarePositive(){
         Square square = SquareFactory.newSquare(0.5);
         assertEquals("Expected value is not equal to actual", 0.25, square.square(), DELTA);
     }
@@ -45,19 +30,10 @@ public class TestSquare {
         assertEquals("Expected value is not equal to actual", 0.0, square.square(), DELTA);
     }
 
-    /*
-     In my opinion, test expected result should be some Exception, if provided sideLength is negative. Nevertheless, it should be specified in specification
-     */
-    @Test(expected = Exception.class)
-    public void checkSquareOfSquareNegative(){
-        Square square = SquareFactory.newSquare(-25.5);
-        square.square();
-    }
-
     @Test
     public void checkSquaresAreEqualPositive(){
-        Square square1 = SquareFactory.newSquare(9.9);
-        Square square2 = SquareFactory.newSquare(9.9);
+        Square square1 = SquareFactory.newSquare(9.99);
+        Square square2 = SquareFactory.newSquare(9.99);
         assertTrue("Squares aren't equal", square1.equalsSquare(square2));
     }
 
@@ -68,13 +44,4 @@ public class TestSquare {
         assertTrue("Squares aren't equal", square1.equalsSquare(square2));
     }
 
-    /*
-     In my opinion, test expected result should be some Exception, if provided sideLength is negative. Nevertheless, it should be specified in specification
-     */
-    @Test(expected = Exception.class)
-    public void checkSquaresAreEqualNegative(){
-        Square square1 = SquareFactory.newSquare(-0.1);
-        Square square2 = SquareFactory.newSquare(-0.1);
-        square1.equalsSquare(square2);
-    }
 }
